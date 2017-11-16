63c4717cfef5bf4cf0d11fb62153e10d5500df8e
grrrben <grrrben@gmail.com>
2017-11-16 08:30:15 +0100

    base test for the blockchain

--- blockchain_test.go
+++ blockchain_test.go
@@ -5,6 +5,9 @@ import (
 	"flag"
 )
 
+// Testing a couple of blockchain related functions on a base chain
+// with just the genesis block
+
 func init() {
 	// setup
 	flag.Parse()
@@ -34,3 +37,35 @@ func TestInitBlockchain(t *testing.T) {
 		t.Errorf("New blockchain should not have transactions. Got: %d transactions.", len(bc.Transactions))
 	}
 }
+
+func TestNewTransaction (t *testing.T) {
+	transaction := Transaction{
+		"sender",
+		"receiver",
+		1,
+	}
+
+	newBlockIndex := bc.newTransaction(transaction)
+	lastBlockIndex := bc.lastBlock().Index + 1
+
+	if newBlockIndex != lastBlockIndex {
+		t.Errorf("Index of new block fails. Got %d, Want %d", newBlockIndex, lastBlockIndex)
+	}
+}
+
+// TestValidate Current blockchain just has the Genesis block
+// should always be valid
+func TestValidate (t *testing.T) {
+	valid := bc.validate()
+	if !valid {
+		t.Error("Blockchain is invalid.")
+	}
+}
+
+func TestLastBlock (t *testing.T) {
+	block := bc.lastBlock();
+	if block.Index != 1 {
+		t.Errorf("Last block index should be 2, got %d.", block.Index)
+	}
+}
+
